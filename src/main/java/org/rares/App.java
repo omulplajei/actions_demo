6b399c20e3745d8fe9f269f67cf3023f8ebe1bb7
Rares Rusu <rares.rusu@paddypowerbetfair.com>
2020-08-26 12:23:59 +0300

    fix

--- src/main/java/org/rares/App.java
+++ src/main/java/org/rares/App.java
@@ -11,5 +11,6 @@ public class App
 
         System.out.println( "Hello World!" );
         System.out.println( "Hello World again!" );
+        System.out.println( "Hello World again and again!" );
     }
 }
