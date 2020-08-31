fe7cb999108bff2e38307b7325f41fe27204b657
Rares Rusu <rares.rusu@paddypowerbetfair.com>
2020-08-31 12:18:14 +0300

    try error level

--- src/main/java/org/rares/App.java
+++ src/main/java/org/rares/App.java
@@ -13,7 +13,8 @@ public class App
         System.out.println( "Hello World again!" );
         System.out.println( "Hello World again and again!" );
 
-        String variable = null;
-            System.out.println( variable.length() );
+
+        String isNull = null;
+            System.out.println( isNull.length() );
     }
 }
