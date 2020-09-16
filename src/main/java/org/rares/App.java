9ed9b988dc7bd4a99489cdbcc2886ada8e53f6c2
mirceamindru <mircea.mindru@paddypowerbetfair.com>
2020-09-16 14:23:47 +0300

    add second commit without reference.

--- src/main/java/org/rares/App.java
+++ src/main/java/org/rares/App.java
@@ -17,5 +17,6 @@ public class App
         System.out.println(isNull.length());
 
         System.out.println("Testing last commit message check!");
+        System.out.println("Second commit.");
     }
 }
