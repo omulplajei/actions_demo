9bc85f20b21247ec82333dad8cb8a87a202685a1
mirceamindru <mircea.mindru@paddypowerbetfair.com>
2020-09-22 16:35:08 +0300

    #123 - 7th

--- src/main/java/org/rares/App.java
+++ src/main/java/org/rares/App.java
@@ -28,6 +28,7 @@ public class App {
         System.out.println("4th");
         System.out.println("5th");
         System.out.println("6th");
+        System.out.println("7th");
     }
 
     private void privateMethodNoJavadoc() {
